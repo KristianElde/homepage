aafa6280de80412204b6f7411f045dec3a710f4a
Kristian Elde Johansen <kristian.e.johansen@student.uib.no>
2024-09-14 00:06:23 +0200

    change listener for local storage hook

--- src/hooks/useLang.tsx
+++ src/hooks/useLang.tsx
@@ -4,6 +4,7 @@ import { useLocalStorageState } from "ahooks";
 export const useLang = () => {
   const [langStorage, setLangStorage] = useLocalStorageState("lang", {
     defaultValue: "no",
+    listenStorageChange: true,
   });
   const setLang = (lang: "en" | "no") => {
     setLangStorage(lang);
