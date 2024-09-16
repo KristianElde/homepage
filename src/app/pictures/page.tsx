ad854e51dca172204e417f41529496e3c18b39cf
Kristian Elde Johansen <kristian.e.johansen@student.uib.no>
2024-09-16 09:33:02 +0200

    pictures to a module

--- src/app/pictures/page.tsx
+++ src/app/pictures/page.tsx
@@ -1,29 +1,21 @@
-// import Picture from "../../components/picture";
+import Picture from "../../components/picture";
 
-// export default function Page() {
-//   const images: string[] = [
-//     "1.jpg",
-//     "3.jpg",
-//     "6.jpg",
-//     "5.jpg",
-//     "1.jpg",
-//     "3.jpg",
-//     "5.jpg",
-//   ];
-//   return (
-//     <div className="mx-4 pt-8 text-md">
-//       <ul className="space-y-3">
-//         {images.map((image, i) => {
-//           return (
-//             <li
-//               key={i}
-//               className={`flex ${i % 2 == 0 ? "justify-start" : "justify-end"}`}
-//             >
-//               <Picture path={image}></Picture>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </div>
-//   );
-// }
+export default function Page() {
+  const images: string[] = [];
+  return (
+    <div className="mx-4 pt-8 text-md">
+      <ul className="space-y-3">
+        {images.map((image, i) => {
+          return (
+            <li
+              key={i}
+              className={`flex ${i % 2 == 0 ? "justify-start" : "justify-end"}`}
+            >
+              <Picture path={image}></Picture>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
